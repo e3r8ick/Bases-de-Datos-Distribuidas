7789707c3b5ae77136c0ff80374872abb6331fc1
JosafatV <josav09@gmail.com>
2019-03-26 16:40:22 -0600

    Srcipt para crear la base
    Primer iteracion del script para crear la base de datos

---
+++ SQL Scripts/CREATION SCRIPT.sql
@@ -0,0 +1,92 @@
+USE master
+CREATE DATABASE AdvancedInc
+
+USE AdvancedInc 
+
+CREATE TABLE USUARIOS (
+	CodUsuario INT IDENTITY(1,1),
+	Nombre VARCHAR(50),
+	Contrasena VARCHAR(50),
+	Roll CHAR,
+
+	CONSTRAINT PK_CodUsuario 
+		PRIMARY KEY (CodUsuario)
+)
+
+CREATE TABLE DEPARTAMENTOS (
+	CodDepartamento INT IDENTITY(1,1),
+	Nombre VARCHAR(50),
+	Descripcion VARCHAR(50),
+	Estado CHAR,
+	
+	CONSTRAINT PK_CodDepartamento 
+		PRIMARY KEY (CodDepartamento)
+) 
+
+CREATE TABLE SEDES (
+	CodSede INT IDENTITY(1,1),
+	Nombre VARCHAR(50),
+	Descripcion VARCHAR(50),
+	Provincia VARCHAR(50),
+	Canton VARCHAR(50),
+	Distrito VARCHAR(50),
+	DetalleUbicacion VARCHAR(50),
+	Estado CHAR,
+	CodEmpleado INT,
+	FechaIngresoAdmin DATE,
+
+	CONSTRAINT PK_CodSede 
+		PRIMARY KEY (CodSede)
+)
+
+
+CREATE TABLE EMPLEADOS (
+	CodEmpleado INT IDENTITY (1,1),
+	Nombre VARCHAR(50),
+	Estado CHAR,
+	Fotgrafia IMAGE,
+	CodSede INT, 
+	CodDepartamento INT,
+	FechaIngreso DATE,
+	Puesto VARCHAR(50),
+	Cedula INT,
+
+	CONSTRAINT PK_CodEmpleado 
+		PRIMARY KEY (CodEmpleado),
+
+	CONSTRAINT FK_EMPLEADOS_CodSede
+		FOREIGN KEY (CodSede) REFERENCES SEDES(CodSede),
+
+	CONSTRAINT FK_EMPLEADOS_CodDepartamento
+		FOREIGN KEY (CodDepartamento) REFERENCES DEPARTAMENTOS(CodDepartamento)
+);
+
+
+CREATE TABLE ACTIVOS (
+	CodActivo INT IDENTITY (1,1),
+	Nombre VARCHAR(50),
+	Descripcion VARCHAR(50),
+	Categoria VARCHAR(50),
+	Foto IMAGE, 
+	PrecioCompra INT,
+	TiempoGarantia DATE,
+	VidaUtil INT, /*DIAS*/
+	PtjeDepreciacion FLOAT,
+	FechaCompra DATE,
+	FechaRegistro DATE,
+	Estado CHAR,
+	CentroCosto VARCHAR(10),
+	ValorResidual INT,
+	CodSede INT,
+	DetalleUbicacion VARCHAR(200)
+
+	CONSTRAINT PK_CodActivo 
+		PRIMARY KEY (CodActivo),
+
+	CONSTRAINT FK_ACTIVOS_CodSede
+		FOREIGN KEY (CodSede) REFERENCES SEDES(CodSede),
+
+	CONSTRAINT FK_ACTIVOS_CentroCosto
+		FOREIGN KEY (CentroCosto) REFERENCES DEPARTAMENTOS(idDepartamento)
+);
+
