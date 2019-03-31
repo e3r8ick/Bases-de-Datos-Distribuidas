f1a5933f86a4e9548fc2009359b2320e846d8ef2
Erick Cordero Rojas <eguicoro2@gmail.com>
2019-03-30 18:22:15 -0600

    Create reports.php

---
+++ WebClient/reports.php
@@ -0,0 +1,44 @@
+<?php
+// se incluye el header de la pagina
+include 'topmenu.php';
+?>
+
+<!DOCTYPE html>
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/grid.css">
+
+
+<form method="post" action="index.html">
+<div class="box">
+<h1>Asset Assignment Advance Inc</h1>
+
+<div class="column">
+  <div class="grid-item">
+    <h4>Code</h4>
+    <input type="text" name="email" placeholder="Code" class="user" />
+  </div>
+  <div class="grid-item">
+    <h4>Employee ID</h4>
+    <input type="text" name="email" placeholder="Employee ID" class="user" />
+  </div>
+  <div class="grid-item">
+    <h4>Date</h4>
+    <input type="text" name="email" placeholder="Date" class="user" />
+  </div>
+  <div class="grid-item">
+    <h4>Detail</h4>
+    <input type="text" name="email" placeholder="Detail" class="user" />
+  </div>
+  <div class="grid-item">
+    <h4>Venue</h4>
+    <input type="text" name="email" placeholder="Venue" class="user" />
+  </div>
+</div>
+<a href=""><div class="btn">Enter</div></a> <!-- End Btn -->
+  
+</div> <!-- End Box -->
+  
+</form>
+  
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
