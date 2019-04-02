2e67d0f6e69480ea01bc1482c6928cf02a5989e5
luisohz <36829909+luisohz@users.noreply.github.com>
2019-04-02 17:49:40 -0600

    Agrega Stored Procedures a los Scripts

--- SQL Scripts/STORED PROCEDURES USERS.sql
+++ SQL Scripts/STORED PROCEDURES USERS.sql
@@ -1,3 +1,15 @@
+DROP PROCEDURE spCreateUser;
+
+CREATE PROCEDURE spCreateUser 
+	@UserName VARCHAR(50),
+	@UserPassword VARCHAR(50),
+	@UserRol CHAR
+AS
+	INSERT INTO USUARIOS (Nombre, Contrasena, Roll) 
+	VALUES 
+		(@UserName, @UserPassword, @UserRol)
+GO
+
 DROP PROCEDURE spSearchUser;
 
 CREATE PROCEDURE spSearchUser 
