a252d7d4990f2aba715e2fb06ad7bc7476ddc5de
luisohz <36829909+luisohz@users.noreply.github.com>
2019-04-03 12:19:27 -0600

    Agrega los Stored Procedures para Activos

---
+++ SQL Scripts/STORED PROCEDURES ASSETS.sql
@@ -0,0 +1,90 @@
+USE AdvancedInc
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'spCreateAsset')
+	DROP PROCEDURE spCreateAsset
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'spSearchAsset')
+	DROP PROCEDURE spSearchAsset
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'spSearchAllAssets')
+	DROP PROCEDURE spSearchAllAssets
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'spDeleteAsset')
+	DROP PROCEDURE spDeleteAsset
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'spUpdateAsset')
+	DROP PROCEDURE spUpdateAsset
+GO
+
+CREATE PROCEDURE spCreateAsset
+	@AssetName VARCHAR(50),
+	@AssetDescription VARCHAR(50),
+	@AssetCategory VARCHAR(50),
+	@AssetPhoto IMAGE, 
+	@AssetPrice INT,
+	@AssetLifeSpan INT, /*DIAS*/
+	@AssetPjeDepreciacion FLOAT,
+	@AssetBuyingDate DATE,
+	@AssetRegistrationDate DATE,
+	@AssetWarrantyDate DATE,
+	@AssetValorResidual INT,
+	@AssetCentroCosto INT,
+	@AssetCodEmployee INT,
+	@AssetCodVenue INT,
+	@AssetUbicationDetail VARCHAR(200),
+	@AssetStatus CHAR
+AS
+	INSERT INTO ACTIVOS
+		(Nombre, Descripcion, Categoria, Foto, PrecioCompra, VidaUtil, PjeDepreciacion, FechaCompra, FechaRegistro, FechaGarantia, ValorResidual, CentroCosto, CodEmpleado, CodSede, DetalleUbicacion, Estado)
+	VALUES 
+		(@AssetName, @AssetDescription, @AssetCategory, @AssetPhoto, @AssetPrice, @AssetLifeSpan, @AssetPjeDepreciacion, @AssetBuyingDate, @AssetRegistrationDate, @AssetWarrantyDate, @AssetValorResidual, @AssetCentroCosto, @AssetCodEmployee, @AssetCodVenue, @AssetUbicationDetail, @AssetStatus)
+GO
+
+CREATE PROCEDURE spSearchAsset 
+	@AssetCode INT
+AS
+	SELECT * 
+	FROM ACTIVOS 
+	WHERE CodActivo=@AssetCode;
+GO
+
+CREATE PROCEDURE spSearchAllAssets 
+AS
+	SELECT * 
+	FROM ACTIVOS;
+GO
+
+CREATE PROCEDURE spDeleteAsset 
+	@AssetCode VARCHAR(50)
+AS
+	DELETE 
+	FROM ACTIVOS 
+	WHERE CodActivo=@AssetCode;
+GO
+
+CREATE PROCEDURE spUpdateAsset
+	@AssetCode INT,
+	@AssetName VARCHAR(50),
+	@AssetDescription VARCHAR(50),
+	@AssetCategory VARCHAR(50),
+	@AssetPhoto IMAGE, 
+	@AssetPrice INT,
+	@AssetLifeSpan INT, /*DIAS*/
+	@AssetPjeDepreciacion FLOAT,
+	@AssetBuyingDate DATE,
+	@AssetRegistrationDate DATE,
+	@AssetWarrantyDate DATE,
+	@AssetValorResidual INT,
+	@AssetCentroCosto INT,
+	@AssetCodEmployee INT,
+	@AssetCodVenue INT,
+	@AssetUbicationDetail VARCHAR(200),
+	@AssetStatus CHAR
+AS
+	UPDATE ACTIVOS
+	SET 
+		Nombre = @AssetName, Descripcion = @AssetDescription, Categoria = @AssetCategory, Foto = @AssetPhoto, PrecioCompra = @AssetPrice, VidaUtil = @AssetLifeSpan, PjeDepreciacion = @AssetPjeDepreciacion, FechaCompra = @AssetBuyingDate, FechaRegistro = @AssetRegistrationDate, FechaGarantia = @AssetWarrantyDate, ValorResidual = @AssetValorResidual, CentroCosto = @AssetCentroCosto, CodEmpleado = @AssetCodEmployee, CodSede = @AssetCodVenue, DetalleUbicacion = @AssetUbicationDetail, Estado = @AssetStatus
+	WHERE
+		CodActivo = @AssetCode
+GO
